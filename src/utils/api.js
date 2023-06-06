85ace416e80adcd1a92c439477da15d67708aa85
DostonDev <dastondev0121@gmail.com>
2023-06-06 19:29:01 +0500

    full

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":'bd1c7eabf4msh271d81598dc080fp165d3cjsn45d525b29067',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
